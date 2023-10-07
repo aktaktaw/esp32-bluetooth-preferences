c5109300caa2ac087d19fc5387b671fbe59d97c2
aktaktaw <aktaktaw@gmail.com>
2023-10-08 00:55:22 +0800

    update code

--- esp32-preferences-bluetoothSerial/esp32-preferences-bluetoothSerial.ino
+++ esp32-preferences-bluetoothSerial/esp32-preferences-bluetoothSerial.ino
@@ -2,7 +2,6 @@
 #include <BluetoothSerial.h>
 
 const int LED_PIN = 13;
-const int BUTTON_PIN = 12;
 const char* PREF_NAMESPACE = "led_state";
 const char* PREF_KEY = "state";
 
@@ -14,8 +13,7 @@ bool currentState = false;
 void setup() {
   Serial.begin(115200);
   pinMode(LED_PIN, OUTPUT);
-  pinMode(BUTTON_PIN, INPUT_PULLUP);
-
+  
   preferences.begin(PREF_NAMESPACE, false);
   currentState = preferences.getBool(PREF_KEY, false);
   digitalWrite(LED_PIN, currentState ? HIGH : LOW);
@@ -30,25 +28,6 @@ void setup() {
 }
 
 void loop() {
-  // Check if the button is pressed
-  if (digitalRead(BUTTON_PIN) == LOW) {
-    currentState = !currentState;
-    digitalWrite(LED_PIN, currentState ? HIGH : LOW);
-    preferences.putBool(PREF_KEY, currentState);
-    preferences.end();
-    Serial.println(currentState ? "LED is ON" : "LED is OFF");
-
-    // Send state over Bluetooth
-    if (SerialBT.hasClient()) {
-      SerialBT.println(currentState ? "ON" : "OFF");
-    }
-
-    // Wait for button release
-    while (digitalRead(BUTTON_PIN) == LOW) {
-      delay(10);
-    }
-  }
-
   // Check for Bluetooth commands
   if (SerialBT.available()) {
     String command = SerialBT.readStringUntil('\n');
